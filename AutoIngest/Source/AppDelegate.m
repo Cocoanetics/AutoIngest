Cocoanetics/AutoIngest

commit 6a2b957cccd0ff7b0b88c3ff33be0ae1b75bddc4

added cancellation of sync and corresponding menu item title

AutoIngest/Source/AppDelegate.m
@@ -14,6 +14,15 @@
 
 #import "StatusItemView.h"
 
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSMenuItem *syncMenuItem;
+
+
+@end
+
+
 @implementation AppDelegate
 {
 	NSStatusItem *_statusItem;
@@ -50,6 +59,8 @@
 	statusItemView.statusItem = _statusItem;
     statusItemView.menu = _statusMenu;
     [_statusItem setView:statusItemView];
+    
+    _syncMenuItem.title = NSLocalizedString(@"Sync now", nil);
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
@@ -77,10 +88,21 @@
 	return YES;
 }
 
-- (void)syncNow:(id)sender
+- (IBAction)syncMenuItemAction:(id)sender
 {
-    [[DTITCReportManager sharedManager] startSync];
+    DTITCReportManager *reportManager = [DTITCReportManager sharedManager];
+    if (reportManager.isSynching)
+    {
+        [reportManager stopSync];
+        _syncMenuItem.title = NSLocalizedString(@"Sync now", nil);
+    }
+    else
+    {
+        [reportManager startSync];
+        _syncMenuItem.title = NSLocalizedString(@"Stop sync", nil);
+    }    
 }
+
 
 - (void)quitApplication:(id)sender
 {

AutoIngest/Resources/MainMenu.xib
@@ -101,14 +101,6 @@
 				</object>
 				<object class="IBConnectionRecord">
 					<object class="IBActionConnection" key="connection">
-						<string key="label">syncNow:</string>
-						<reference key="source" ref="1014"/>
-						<reference key="destination" ref="1050566657"/>
-					</object>
-					<int key="connectionID">544</int>
-				</object>
-				<object class="IBConnectionRecord">
-					<object class="IBActionConnection" key="connection">
 						<string key="label">terminate:</string>
 						<reference key="source" ref="1014"/>
 						<reference key="destination" ref="333196037"/>
@@ -122,6 +114,22 @@
 						<reference key="destination" ref="800806550"/>
 					</object>
 					<int key="connectionID">541</int>
+				</object>
+				<object class="IBConnectionRecord">
+					<object class="IBOutletConnection" key="connection">
+						<string key="label">syncMenuItem</string>
+						<reference key="source" ref="976324537"/>
+						<reference key="destination" ref="1050566657"/>
+					</object>
+					<int key="connectionID">546</int>
+				</object>
+				<object class="IBConnectionRecord">
+					<object class="IBActionConnection" key="connection">
+						<string key="label">syncMenuItemAction:</string>
+						<reference key="source" ref="976324537"/>
+						<reference key="destination" ref="1050566657"/>
+					</object>
+					<int key="connectionID">547</int>
 				</object>
 			</array>
 			<object class="IBMutableOrderedSet" key="objectRecords">
@@ -204,24 +212,27 @@
 			<nil key="activeLocalization"/>
 			<dictionary class="NSMutableDictionary" key="localizations"/>
 			<nil key="sourceID"/>
-			<int key="maxID">545</int>
+			<int key="maxID">547</int>
 		</object>
 		<object class="IBClassDescriber" key="IBDocument.Classes">
 			<array class="NSMutableArray" key="referencedPartialClassDescriptions">
 				<object class="IBPartialClassDescription">
 					<string key="className">AppDelegate</string>
 					<string key="superclassName">NSObject</string>
-					<object class="NSMutableDictionary" key="outlets">
-						<string key="NS.key.0">statusMenu</string>
-						<string key="NS.object.0">NSMenu</string>
-					</object>
-					<object class="NSMutableDictionary" key="toOneOutletInfosByName">
-						<string key="NS.key.0">statusMenu</string>
-						<object class="IBToOneOutletInfo" key="NS.object.0">
+					<dictionary class="NSMutableDictionary" key="outlets">
+						<string key="statusMenu">NSMenu</string>
+						<string key="syncMenuItem">NSMenuItem</string>
+					</dictionary>
+					<dictionary class="NSMutableDictionary" key="toOneOutletInfosByName">
+						<object class="IBToOneOutletInfo" key="statusMenu">
 							<string key="name">statusMenu</string>
 							<string key="candidateClassName">NSMenu</string>
 						</object>
-					</object>
+						<object class="IBToOneOutletInfo" key="syncMenuItem">
+							<string key="name">syncMenuItem</string>
+							<string key="candidateClassName">NSMenuItem</string>
+						</object>
+					</dictionary>
 					<object class="IBClassDescriptionSource" key="sourceIdentifier">
 						<string key="majorKey">IBProjectSource</string>
 						<string key="minorKey">./Classes/AppDelegate.h</string>

AutoIngest/Source/DTITCReportManager.h
@@ -13,6 +13,8 @@ extern NSString * const DTITCReportManagerSyncDidFinishNotification;
 
 @interface DTITCReportManager : NSObject
 
+@property (nonatomic) BOOL isSynching;
+
 + (DTITCReportManager *)sharedManager;
 
 - (void)startSync;

AutoIngest/Source/DTITCReportManager.m
@@ -24,9 +24,7 @@ NSString * const DTITCReportManagerSyncDidFinishNotification = @"DTITCReportMana
 @end
 
 @implementation DTITCReportManager
-{
-	BOOL _synching;
-	
+{	
 	NSString *_reportFolder;
 	NSString *_vendorID;
 	
@@ -93,7 +91,7 @@ NSString * const DTITCReportManagerSyncDidFinishNotification = @"DTITCReportMana
 
 - (void)startSync
 {
-	if (_synching)
+	if (_isSynching)
 	{
 		NSLog(@"Already Synching");
 		return;
@@ -171,7 +169,7 @@ NSString * const DTITCReportManagerSyncDidFinishNotification = @"DTITCReportMana
 	[_queue addOperationWithBlock:^{
 		[weakself _reportCompletionWithError:_error];
 		
-		_synching = NO;
+		_isSynching = NO;
 	}];
 	
 	if (!hasWorkToDo)
@@ -186,12 +184,12 @@ NSString * const DTITCReportManagerSyncDidFinishNotification = @"DTITCReportMana
 	
 	[_queue setSuspended:NO];
 	
-	_synching = YES;
+	_isSynching = YES;
 }
 
 - (void)stopSync
 {
-	if (!_synching)
+	if (!_isSynching)
 	{
 		return;
 	}
@@ -213,7 +211,7 @@ NSString * const DTITCReportManagerSyncDidFinishNotification = @"DTITCReportMana
 	[_queue setSuspended:NO];
 
 	
-	_synching = NO;
+	_isSynching = NO;
 }
 
 - (BOOL)canSync
@@ -333,7 +331,7 @@ NSString * const DTITCReportManagerSyncDidFinishNotification = @"DTITCReportMana
 	}
 	
 	// need to stop sync, folder changed
-	if (_synching && needsToStopSync)
+	if (_isSynching && needsToStopSync)
 	{
 		[self stopSync];
 	}

commit fd5d6dd5eadec0dc63625d82d8532f751c77b5a4

fixed menu item validation, broken by 6a2b957cccd0

AutoIngest/Source/AppDelegate.m
@@ -127,7 +127,7 @@
 
 - (BOOL)validateMenuItem:(NSMenuItem *)menuItem
 {
-	if (menuItem.action == @selector(syncNow:))
+	if (menuItem.action == @selector(syncMenuItemAction:))
 	{
 		return [[DTITCReportManager sharedManager] canSync];
 	}
@@ -145,6 +145,12 @@
     }
     else
     {
+		if (![reportManager canSync])
+		{
+			NSLog(@"Report Manager reports unable to sync");
+			return;
+		}
+		
         [reportManager startSync];
         _syncMenuItem.title = NSLocalizedString(@"Stop sync", nil);
     }    